fa27542fb04ef453ec6753afc4c4b69a46e3c14b
DavidKD5 <jdbetancourt6@gmail.com>
2022-06-27 22:24:31 -0600

    logic to save event is complete

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,14 @@
 var eventInput = $(".eventInput");
-var saveBtn = $(".saveBtn");
+var eventInput2 = $(".eventInput2");
+var eventInput3 = $(".eventInput3");
+var eventInput4 = $(".eventInput4");
+var eventInput5 = $(".eventInput5");
+var eventInput6 = $(".eventInput6");
+var eventInput7 = $(".eventInput7");
+var eventInput8 = $(".eventInput8");
+var eventInput9 = $(".eventInput9");
+var btn = $(".btn");
+
 var savedEvent = localStorage.getItem("saveEvent");
 var savedEvent2 = localStorage.getItem("saveEvent2");
 var savedEvent3 = localStorage.getItem("saveEvent3");
@@ -10,84 +19,74 @@ var savedEvent7 = localStorage.getItem("saveEvent7");
 var savedEvent8 = localStorage.getItem("saveEvent8");
 var savedEvent9 = localStorage.getItem("saveEvent9");
 
-var allBtn = [
-  $(".saveBtn1"),
-  $(".saveBtn2"),
-  $(".saveBtn3"),
-  $(".saveBtn4"),
-  $(".saveBtn5"),
-  $(".saveBtn6"),
-  $(".saveBtn7"),
-  $(".saveBtn8"),
-  $(".saveBtn9"),
-];
-
-var allInput = [
-  $(".eventInput1"),
-  $(".eventInput2"),
-  $(".eventInput3"),
-  $(".eventInput4"),
-  $(".eventInput5"),
-  $(".eventInput6"),
-  $(".eventInput7"),
-  $(".eventInput8"),
-  $(".eventInput9"),
-];
-
-0.03 + 2615 - 7;
-var allEvents = [
-  saveEvent,
-  saveEvent2,
-  saveEvent3,
-  saveEvent4,
-  saveEvent5,
-  saveEvent6,
-  saveEvent7,
-  saveEvent8,
-  saveEvent9,
-];
-
 function saveEvent() {
-  localStorage.setItem("saveEvent", $(".eventInput").val());
-  alert(savedEvent);
+  var eventInput = $(".eventInput");
+  localStorage.setItem("saveEvent", eventInput.val());
+  alert("Event Saved To Local Storage!");
 }
+$(".saveBtn").click(saveEvent);
+eventInput.val(savedEvent);
+
 function saveEvent2() {
-  localStorage.setItem("saveEvent2", $(".eventInput2").val());
-  alert(savedEvent2);
+  var eventInput2 = $(".eventInput2");
+  localStorage.setItem("saveEvent2", eventInput2.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn2").click(saveEvent2);
+eventInput2.val(savedEvent2);
+
 function saveEvent3() {
-  localStorage.setItem("saveEvent3", $(".eventInput3").val());
-  alert(savedEvent3);
+  var eventInput3 = $(".eventInput3");
+  localStorage.setItem("saveEvent3", eventInput3.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn3").click(saveEvent3);
+eventInput3.val(savedEvent3);
+
 function saveEvent4() {
-  localStorage.setItem("saveEvent4", $(".eventInput4").val());
-  alert(savedEvent4);
+  var eventInput4 = $(".eventInput4");
+  localStorage.setItem("saveEvent4", eventInput4.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn4").click(saveEvent4);
+eventInput4.val(savedEvent4);
+
 function saveEvent5() {
-  localStorage.setItem("saveEvent5", $(".eventInput5").val());
-  alert(savedEvent5);
+  var eventInput5 = $(".eventInput5");
+  localStorage.setItem("saveEvent5", eventInput5.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn5").click(saveEvent5);
+eventInput5.val(savedEvent5);
+
 function saveEvent6() {
-  localStorage.setItem("saveEvent6", $(".eventInput6").val());
-  alert(savedEvent6);
+  var eventInput6 = $(".eventInput6");
+  localStorage.setItem("saveEvent6", eventInput6.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn6").click(saveEvent6);
+eventInput6.val(savedEvent6);
+
 function saveEvent7() {
-  localStorage.setItem("saveEvent7", $(".eventInput7").val());
-  alert(savedEvent7);
+  var eventInput7 = $(".eventInput7");
+  localStorage.setItem("saveEvent7", eventInput7.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn7").click(saveEvent7);
+eventInput7.val(savedEvent7);
+
 function saveEvent8() {
-  localStorage.setItem("saveEvent8", $(".eventInput8").val());
-  alert(savedEvent8);
+  var eventInput8 = $(".eventInput8");
+  localStorage.setItem("saveEvent8", eventInput8.val());
+  alert("Event Saved To Local Storage");
 }
+$(".saveBtn8").click(saveEvent8);
+eventInput8.val(savedEvent8);
+
 function saveEvent9() {
-  localStorage.setItem("saveEvent9", $(".eventInput9").val());
-  alert(savedEvent9);
+  var eventInput9 = $(".eventInput9");
+  localStorage.setItem("saveEvent9", eventInput9.val());
+  alert("Event Saved To Local Storage");
 }
-
-for (var i = 0; i < allBtn.length; i++) {
-  allBtn[i].click(saveEvent);
-}
-
-if (savedEvent) {
-  eventInput.val(savedEvent);
-}
+$(".saveBtn9").click(saveEvent9);
+eventInput9.val(savedEvent9);
